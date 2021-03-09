68cefdb5d94c7e5dbbc2ca9dc61a4c71de7570c9
nickloverde <nick.loverde@gmail.com>
2021-03-09 12:27:36 -0600

    set up old school boiler plate for redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
 import Posts from './components/Posts'
 import Postform from './components/Postform'
+import { createStore, applyMiddleware} from 'redux'
+
+import store from './store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <Postform />
       <hr />
       <Posts />
     </div>
+    </Provider>
   );
 }
 
