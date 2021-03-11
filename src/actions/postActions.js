763f0a55bde2ea5a40c1bb1d57c53b9ad035ca27
nickloverde <nick.loverde@gmail.com>
2021-03-10 19:46:59 -0600

    added payload

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -2,6 +2,11 @@ import { FETCH_POSTS, NEW_POST } from './types'
 
 export function fetchPosts() {
     return function(dispatch) {
-         
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts => dispatch({
+            type: FETCH_POSTS,
+            payload: posts
+        }))
     }
 }
