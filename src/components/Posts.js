01a831b5e55aaf2c2e303f5400830ec77471bec4
nickloverde <nick.loverde@gmail.com>
2021-03-12 17:10:47 -0600

    added mapstatetoprops

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions/postActions'
 
 class Posts extends Component {
 
+    componentDidMount () {
+        this.props.fetchPosts()
+    }
+
     render() {
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -18,4 +24,8 @@ class Posts extends Component {
     }
 }
 
-export default Posts
+const mapStateToProps = state => ({
+    posts: state.posts.items
+})
+
+export default connect(null, { fetchPosts })(Posts)
