8e31ff6014e8e48eb6a38c75037ea6a0cd2a942b
nickloverde <nick.loverde@gmail.com>
2021-03-12 17:18:15 -0600

    wrapped middleware with compose

--- src/store.js
+++ src/store.js
@@ -6,6 +6,11 @@ const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
+const store = createStore(
+    rootReducer, 
+    initialState, 
+    compose(
+    applyMiddleware(...middleware))
+)
 
 export default store 
